10be6c5957722fb1adfb89a5853407f6acf3f080
Moussa Niang <niangm@ept.sn>
2023-11-06 15:38:17 +0000

    Modified manifest files and created waiter-job for deployments

--- apply.sh
+++ apply.sh
@@ -4,13 +4,19 @@ cd Kubernetes
 kubectl apply -f config-server-deployment.yaml
 kubectl apply -f config-server-service.yaml
 
-kubectl apply -f mproduits-deployment.yaml
-kubectl apply -f mproduits-service.yaml
+kubectl apply -f waiter-job.yaml
 
-kubectl apply -f mcommandes-deployment.yaml
-kubectl apply -f mcommandes-service.yaml
-
-kubectl apply -f mpaiement-deployment.yaml
-kubectl apply -f mpaiement-service.yaml
+kubectl apply -f deployments.yaml
+kubectl apply -f services.yaml
 
 kubectl apply -f ingress-controller.yaml
+
+# kubectl apply -f mproduits-deployment.yaml
+# kubectl apply -f mproduits-service.yaml
+
+# kubectl apply -f mcommandes-deployment.yaml
+# kubectl apply -f mcommandes-service.yaml
+
+# kubectl apply -f mpaiement-deployment.yaml
+# kubectl apply -f mpaiement-service.yaml
+
