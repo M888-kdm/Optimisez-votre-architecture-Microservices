f98deb12b465d4d3236880ac23daa6d36d32f303
Moussa Niang <niangm@ept.sn>
2023-11-07 08:32:40 +0000

    .

--- apply.sh
+++ apply.sh
@@ -4,7 +4,7 @@ cd Kubernetes
 kubectl apply -f config-server-deployment.yaml
 kubectl apply -f config-server-service.yaml
 
-kubectl apply -f waiter-job.yaml
+kubectl apply -f configmap.yaml
 
 kubectl apply -f deployments.yaml
 kubectl apply -f services.yaml
