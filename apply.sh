4306dfbe9219547b216aa73fe2876b450381677b
Moussa Niang <niangm@ept.sn>
2023-11-08 11:34:57 +0000

    Sync Workflow

--- apply.sh
+++ apply.sh
@@ -9,7 +9,7 @@ cd Kubernetes
 kubectl apply -f config-server-deployment.yaml
 kubectl apply -f config-server-service.yaml
 
-sleep 25
+sleep 10
 
 kubectl apply -f deployments.yaml
 kubectl apply -f services.yaml
