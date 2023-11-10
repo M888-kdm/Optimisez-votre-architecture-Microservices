843471f40669074abe4e7a56acb49627cfd4c8ca
Moussa Niang <niangm@ept.sn>
2023-11-10 09:10:36 +0000

    Added server-side caching

--- ApolloServer/dist/resolvers.js
+++ ApolloServer/dist/resolvers.js
@@ -1,16 +1,16 @@
 export const resolvers = {
     Query: {
         listeCommandes: async (_, __, { dataSources }) => {
-            return dataSources.commandeAPI.getObjects();
+            return dataSources.commandesAPI.getObjects();
         },
         getCommande: async (_, { id }, { dataSources }) => {
-            return dataSources.commandeAPI.getObjectById(id);
+            return dataSources.commandesAPI.getObjectById(id);
         },
         produits: async (_, { id }, { dataSources }) => {
-            return dataSources.produitsAPI.getProduits();
+            return dataSources.produitsAPI.getObjects();
         },
         produitById: async (_, { id }, { dataSources }) => {
-            return dataSources.produitsAPI.getProduitById(id);
+            return dataSources.produitsAPI.getObjectById(id);
         },
     },
     Mutation: {
@@ -24,13 +24,13 @@ export const resolvers = {
     Commande: {
         product: async (parent, args, { dataSources }) => {
             const productId = parent.productId;
-            return dataSources.produitAPI.getObjectById(productId);
+            return dataSources.produitsAPI.getObjectById(productId);
         }
     },
     Paiement: {
         commande: async (parent, args, { dataSources }) => {
             const commandeId = parent.commandeId;
-            return dataSources.commandeAPI.getObjectById(commandeId);
+            return dataSources.commandesAPI.getObjectById(commandeId);
         }
     }
 };
