26bb57411f09d87831693bc34202f4f8c0b9bd1a
Moussa Niang <niangm@ept.sn>
2023-11-21 09:52:55 +0000

    Created dev profile in m-produits

--- microservice-produits/src/main/java/com/mproduits/model/Product.java
+++ microservice-produits/src/main/java/com/mproduits/model/Product.java
@@ -6,45 +6,179 @@ import jakarta.persistence.*;
 @Table(name = "product")
 public class Product {
 
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id")
+//    private int id;
+//
+//    @Column(name = "titre")
+//    private String titre;
+//
+//    @Column(name = "description")
+//    private String description;
+//
+//    @Column(name = "image")
+//    private String image;
+//
+//    @Column(name = "prix")
+//    private Double prix;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitre() {
+//        return titre;
+//    }
+//
+//    public void setTitre(String titre) {
+//        this.titre = titre;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public String getImage() {
+//        return image;
+//    }
+//
+//    public void setImage(String image) {
+//        this.image = image;
+//    }
+//
+//    public Double getPrix() {
+//        return prix;
+//    }
+//
+//    public void setPrix(Double prix) {
+//        this.prix = prix;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Product{" +
+//                "id=" + id +
+//                ", titre='" + titre + '\'' +
+//                ", description='" + description + '\'' +
+//                ", image='" + image + '\'' +
+//                ", prix=" + prix +
+//                '}';
+//    }
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
+    private Long id;  // Assuming ID is a numeric type, adjust accordingly
 
-    @Column(name = "titre")
-    private String titre;
+    @Column(name = "name")
+    private String name;
 
-    @Column(name = "description")
-    private String description;
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "price")
+    private String price;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "about")
+    private String about;
+
+    @Column(name = "product_specification")
+    private String productSpecification;
+
+    @Column(name = "technical_details")
+    private String technicalDetails;
+
+    @Column(name = "weight")
+    private String weight;
 
     @Column(name = "image")
     private String image;
 
-    @Column(name = "prix")
-    private Double prix;
+    @Column(name = "url")
+    private String url;
 
-    public int getId() {
+    @Column(name = "is_amazon_seller")
+    private String isAmazonSeller;
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getTitre() {
-        return titre;
+    public String getName() {
+        return name;
     }
 
-    public void setTitre(String titre) {
-        this.titre = titre;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getDescription() {
-        return description;
+    public String getCategory() {
+        return category;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getProductSpecification() {
+        return productSpecification;
+    }
+
+    public void setProductSpecification(String productSpecification) {
+        this.productSpecification = productSpecification;
+    }
+
+    public String getTechnicalDetails() {
+        return technicalDetails;
+    }
+
+    public void setTechnicalDetails(String technicalDetails) {
+        this.technicalDetails = technicalDetails;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
     }
 
     public String getImage() {
@@ -55,24 +189,19 @@ public class Product {
         this.image = image;
     }
 
-    public Double getPrix() {
-        return prix;
+    public String getUrl() {
+        return url;
     }
 
-    public void setPrix(Double prix) {
-        this.prix = prix;
+    public void setUrl(String url) {
+        this.url = url;
     }
 
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", titre='" + titre + '\'' +
-                ", description='" + description + '\'' +
-                ", image='" + image + '\'' +
-                ", prix=" + prix +
-                '}';
+    public String getIsAmazonSeller() {
+        return isAmazonSeller;
     }
 
-
+    public void setIsAmazonSeller(String isAmazonSeller) {
+        this.isAmazonSeller = isAmazonSeller;
+    }
 }
