b73cd519493a68604e2f856f803fa26b0da21c52
Moussa Niang <niangm@ept.sn>
2023-11-21 12:19:54 +0000

    Added database configurations in AuthServer for prod environment

--- ApolloServer/src/data_sources/produits.ts
+++ ApolloServer/src/data_sources/produits.ts
@@ -1,7 +1,7 @@
 import { Produit } from '../model.js';
 import { BaseDataSource } from './base.js';
 
-export class ProduitsDataSource extends BaseDataSource<Produit> {
+export class ProduitsDataSource extends BaseDataSource<Object> {
 
   constructor(){
       const port = 30001;
